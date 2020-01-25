a5b569dbaaa34d59f7e0e89e30757e9a3b2b17ba
mezan <mrmezan0@gmail.com>
2020-01-26 01:46:44 +0600

    kill application feature added!!!!!!!!

--- Assistant.py
+++ Assistant.py
@@ -4,6 +4,7 @@ import pyttsx3
 import wikipedia
 import webbrowser
 import os
+import psutil
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -65,40 +66,44 @@ def Action_Perform(query):
     if 'wikipedia' in query:
         show_result('Searching Wikipedia...')
         query = query.replace('wikipedia', '')
-        result = wikipedia.summary(query, sentences=2)
+        result = wikipedia.summary(query, sentences=10)
         show_result('According to wikipedia:')
         show_result(result)
     elif 'youtube' in query:
         query = query.replace('youtube', '')
         show_result('Search youtube...')
         browser_open('https://www.youtube.com/results?search_query='+query)
-        exit(0)
+
     elif 'stackoverflow' in query:
         query = query.replace('stackoverflow', '')
         show_result('search stackoverflow...')
         browser_open('https://stackoverflow.com/search?q='+query)
-        exit(0)
+
     elif 'open facebook' in query:
         show_result('Opening Facebook')
         browser_open('www.facebook.com')
-        exit(0)
+
     elif 'google' in query:
         query = query.replace('google', '')
         show_result('Searching Google...')
         browser_open('https://www.google.com/search?q='+query)
-        exit(0)
+
     elif 'play music' in query:
         music_dir = "D:\\Shahid"
         songs = os.listdir(music_dir)
         show_result("Opening music...")
         os.startfile(os.path.join(music_dir, songs[0]))
-        exit_app('Enjoy Music Sir...')
+        show_result('Enjoy Music Sir...')
     elif 'play video' in query:
         music_dir = "F:\\Download"
         songs = os.listdir(music_dir)
         show_result("Opening Video...")
         os.startfile(os.path.join(music_dir, songs[0]))
-        exit_app('Enjoy Video Sir...')
+        show_result('Enjoy Video Sir...')
+    elif 'kill video' in query:
+        for process in (process for process in psutil.process_iter() if process.name() == "PotPlayerMini64.exe"):
+            process.kill()
+            show_result('Killing Video, Sir')
     elif 'show time' in query:
         strTime = datetime.datetime.now().strftime("%I:%M:%S %p")
         show_result(strTime)
@@ -108,6 +113,11 @@ def Action_Perform(query):
         # Opening Specific Software
         path = "C:\\Program Files\\qBittorrent\\qbittorrent.exe"
         os.startfile(path)
+        show_result('Opening qBittorent, Sir')
+    elif 'kill torrent' in query:
+        for process in (process for process in psutil.process_iter() if process.name() == "qbittorrent.exe"):
+            process.kill()
+            show_result('Killing Torrent, Sir')
     elif 'thanks' in query:
         show_result('Welcome. Sir')
     elif 'thank you' in query:
@@ -120,6 +130,19 @@ def Action_Perform(query):
         exit_app("Good Bye! Sir")
     elif 'good night' in query:
         exit_app('Good Night Darling. Have a nice dream! Bye')
+    elif 'option' in query:
+        show_result("Search Wikipedia ...\nOpen Youtube...\nOpen StackOverflow\nOpen Facebook\nSearch Google")
+        show_result("Play Music\nPlay Video\nShow Time\nWho Are You?\nOpen Torrent\nthanks | thank you")
+        show_result("Kill Music\nKill Video\nKill Chrome\nKill Torrent")
+        show_result("Stop|Terminate|Bye|Good Night")
+    elif 'kill music' in query:
+        for process in (process for process in psutil.process_iter() if process.name() == "AIMP.exe"):
+            process.kill()
+            show_result('Killing AIMP, Sir')
+    elif 'kill chrome' in query:
+        for process in (process for process in psutil.process_iter() if process.name() == "chrome.exe"):
+            process.kill()
+            show_result('Killing Chrome, Sir')
 
 
 if __name__ == '__main__':
